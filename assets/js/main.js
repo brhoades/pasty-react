b3e44c6be6b65a97c3746d0d6dfa56d4b7d211d7
Billy J Rhoades II <billyarhoades@gmail.com>
2017-03-24 09:23:31 -0500

    Server now validates urls before reading.
    Properly writes files now, not storing 'undefined'.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ const algorithm = 'aes-256-ctr';
 
 requirejs(["assets/js/libs/crypto-js/crypto-js.js"], (cjs) => {
   CryptoJS = cjs;
+  getFileFromURL();
 });
 
 // https://jsfiddle.net/Guffa/DDn6W/
@@ -29,21 +30,51 @@ function encryptFile(e) {
   console.log(`Encrypted: ${crypted}`);
   console.log(`Password: ${password}`);
 
-  return crypted;
+  return {
+    data: crypted,
+    key: password
+  };
 }
 
-function uploadFile(data) {
+function decryptFile(data, key) {
+  console.log("Decrypted data", CryptoJS.AES.decrypt(data, key));
+}
+
+function uploadFile(crypted_data, cb) {
   console.log("Upload file");
 
   $.ajax({
     type: "POST",
     url: "http://localhost:3000/paste",
     data: {
-      data: data
+      data: crypted_data.data
     },
-    success: (response) => {
-      console.log(response);
-    }
+    success: (response) => cb(response, crypted_data.key)
+  });
+}
+
+function getFile(id, cb) {
+  $.ajax({
+    type: "GET",
+    url: `http://localhost:3000/get/${id}`,
+    success: cb
+  });
+}
+
+// call on doc ready
+// gets the file from the url, gets data from the server,
+// decrypts it, then downloads it
+function getFileFromURL() {
+  let match = /\/view.html\#([^-]+)-(.+)$/.exec(window.location.href);
+  if(!match) {
+    return;
+  }
+
+  let file = match[1],
+      key = decodeURIComponent(match[2]);
+
+  getFile(file, (response) => {
+    decryptFile(response.body.data, key);
   });
 }
 
@@ -54,7 +85,9 @@ $("#filename").fileReaderJS({
     progress: (e, file) => null,
     error: (e, file) => console.log("ERROR"),
     load: (e, file) => {
-      uploadFile(encryptFile(e));
+      uploadFile(encryptFile(e), (res, key) => {
+        window.location.href = res.url + encodeURIComponent(key);
+      });
     }
   }
 });
--- server/main.js
+++ server/main.js
@@ -23,22 +23,26 @@ function randomName(length) {
     return pass;
 }
 
-// Gets a unique filename that doesn't exist.
-// Returns a hash of the name and full path to the file.
-function getFilename() {
+function buildFilename(name) {
   const storage_path = config.get("server.storage.local");
-  let full_path = "";
-  let name = "";
-
   if(!fs.existsSync(storage_path)) {
     console.log(`"${storage_path}" does not exist, cannot create files in it.`);
     return;
   }
 
+  return path.join(storage_path, name);
+}
+
+// Gets a unique filename that doesn't exist.
+// Returns a hash of the name and full path to the file.
+function getFilename() {
+  let full_path = "";
+  let name = "";
+
   do {
     name = randomName(config.get("server.storage.filename_length"));
 
-    full_path = path.join(storage_path, name);
+    full_path = buildFilename(name);
   } while(fs.existsSync(full_path));
 
   return {
@@ -47,10 +51,11 @@ function getFilename() {
   };
 }
 
-// POST x-www-form-urlencoded with a data field
+// POST json with a data field
 // Returns a JSON hash with "filename": "file name".
 app.post("/paste", (req, res) => {
   let data = req.body.data;
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   if(data.length > filesizeParser(config.get("server.storage.size_limit"))) {
     res.status(413);
@@ -63,10 +68,48 @@ app.post("/paste", (req, res) => {
     return;
   }
 
-  fs.writeFileSync(file_details.path, data.body);
+  fs.writeFileSync(file_details.path, data);
 
   res.send({
-    filename: file_details.name
+    filename: file_details.name,
+    url: `${config.get("server.storage.external")}#${file_details.name}-`
+  });
+});
+
+app.get("/get/:file", (req, res, next) => {
+  let file = req.params.file;
+  let file_path = buildFilename(file);
+
+  if(file.length != config.get("server.storage.filename_length")) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    console.log(`${req.params.file} != config length`);
+    return res.status(404) && next();
+  }
+
+  if(/[^A-Za-z0-9]/.exec(file) != null) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    console.log(`${req.params.file} has bad characters`);
+    return res.status(404) && next();
+  }
+
+  fs.exists(file_path, (exists) => {
+    if(!exists) {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      console.log(`File ${file_path} does not exist.`);
+      return res.status(404) && next();
+    }
+
+    fs.readFile(file_path, (err, data) => {
+      if(err) {
+        console.log(`Error in reading: ${err}`);
+        return res.status(500) && next(err);
+      }
+
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.send(data.toString('base64'));
+      return next();
+
+    });
   });
 });
 

c59645fc0d35fb89c1d2ae46276fcedcffd09a73
Billy Rhoades <billyarhoades@gmail.com>
2017-04-21 19:58:46 -0500

    Removed many symbols that break url regexes.

--- src/js/util.ts
+++ src/js/util.ts
@@ -1,18 +1,19 @@
 import $ from "jquery"
 
 // https://jsfiddle.net/Guffa/DDn6W/
-function randomPassword(length) {
-  let chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+<>ABCDEFGHIJKLMNOP1234567890";
-  let pass = "";
+function randomPassword(length): string {
+  let chars: string = "abcdefghijklmnopqrstuvwxyz-_.ABCDEFGHIJKLMNOP1234567890";
+  let pass: string = "";
 
   for (var x = 0; x < length; x++) {
-    var i = Math.floor(Math.random() * chars.length);
+    let i: number = Math.floor(Math.random() * chars.length);
     pass += chars.charAt(i);
   }
+
   return pass;
 }
 
-function getConfig(cb) {
+function getConfig(cb): void {
   console.log("Get Configuration");
 
   $.ajax({

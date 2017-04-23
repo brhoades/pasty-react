ac3570d98bd4ea4e02b2ecd24653b242d43437c4
Billy Rhoades <billyarhoades@gmail.com>
2017-04-23 11:33:37 -0500

    Config is now builtin to the client (as js)

---
+++ config.example.ts
@@ -0,0 +1,6 @@
+let configuration = {
+  paste: "http://localhost:3000/paste",
+  get: "http://localhost:3000/get/"
+};
+
+export default configuration;
--- src/js/client.ts
+++ src/js/client.ts
@@ -1,47 +1,44 @@
 import { getConfig, randomPassword } from "./util"
 import { crypto } from "./crypto"
+import config from "./config"
 import UploadedFile from "./uploadedfile"
 import CodeFile from "./codefile"
 declare var $: any;
 
 function uploadFile(crypted_data, state, cb) {
-   state.message("Getting configuration...");
+  state.message("Uploading...");
 
-   getConfig((config) => {
-      state.message("Uploading...");
-      $.ajax({
-         type: "POST",
-         url: config.paste,
-         data: {
-            data: crypted_data.data
-         },
-         success: (response) => cb(response, crypted_data.key),
-         error: (response) => {
-            return state.message(`Error uploading: ${response}`);
-         }
-      });
-   });
+  $.ajax({
+    type: "POST",
+    url: config.paste,
+    data: {
+      data: crypted_data.data
+    },
+    success: (response) => cb(response, crypted_data.key),
+    error: (response) => {
+      return state.message(`Error uploading: ${response}`);
+    }
+  });
 }
 
 function getFile(id: string, cb) {
-   console.log("Getting file");
-   getConfig((config) => {
-      console.log(`${config.get}${id}`);
-      $.ajax({
-         type: "GET",
-         url: `${config.get}${id}`,
-         success: cb,
-         complete: (xhr, status) => {
-            if (xhr.status == 302) {
-               return $.ajax({
-                  type: "GET",
-                  url: xhr.getResponseHeader("Location"),
-                  success: cb
-               });
-            }
-         }
-      });
-   });
+  console.log("Getting file");
+
+  console.log(`${config.get}${id}`);
+  $.ajax({
+    type: "GET",
+    url: `${config.get}${id}`,
+    success: cb,
+    complete: (xhr, status) => {
+      if (xhr.status == 302) {
+        return $.ajax({
+          type: "GET",
+          url: xhr.getResponseHeader("Location"),
+          success: cb
+        });
+      }
+    }
+  });
 }
 
 export function uploadFileHook(file: any, state: any): void {

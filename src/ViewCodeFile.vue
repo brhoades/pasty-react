0893ebccf90ebabb14b207cbccc37fc5692177dd
Billy Rhoades <billyarhoades@gmail.com>
2017-04-28 18:37:42 -0500

    Code pastes now span the entire content area.

--- src/ViewCodeFile.vue
+++ src/ViewCodeFile.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="padding: 25px; max-width: 800px;">
+  <div>
     <h2>{{ file.name || `unnamed-file-${file.id}` }}</h2>
 
     <a :download=file.name :href=file.base64DownloadString()>Download</a>
@@ -20,3 +20,22 @@
    },
  }
 </script>
+
+<style>
+  pre code {
+    padding-left: 1em !important;
+    padding-right: 1em !important;
+    padding-top: 0.5em !important;
+    padding-bottom: 2em !important;
+    font-family: "Sans Mono", "Consolas", "Courier", monospace;
+
+    overflow: hidden;
+    white-space: pre-wrap;       /* css-3 */
+    white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+    white-space: -pre-wrap;      /* Opera 4-6 */
+    white-space: -o-pre-wrap;    /* Opera 7 */
+    word-wrap: break-word;       /* Internet Explorer 5.5+ */
+  }
+
+
+</style>

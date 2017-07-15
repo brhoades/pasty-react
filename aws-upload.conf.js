973e321c7102ea6bfa1a8164f322c3bc6bf3cdef
Billy Rhoades <billyarhoades@gmail.com>
2017-07-15 12:13:58 -0500

    No longer uploading to s3 under dist/

--- aws-upload.conf.js
+++ aws-upload.conf.js
@@ -1,9 +1,12 @@
 module.exports = {
-  credentials: "aws-credentials.json",
+  credentials: "../aws-credentials.json",
   bucketName: "p.brod.es",
   patterns:[
     "config/*.json",
-    "dist/**/*",
+    "assets/*",
+    "assets/**/*",
+    "*.js",
+    "*.map",
     "*.html"
   ]
 }
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: './',
     filename: '[name].js'
   },
   module: {

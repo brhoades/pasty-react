913644470a7f4096ef9b3fd521b0c0c7ffb202f1
Billy Rhoades <billyarhoades@gmail.com>
2017-07-16 10:26:29 -0500

    Changed about page wording.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,15 +1,15 @@
 # Changelog
 ## NEXT RELEASE
-* TODO: Mobile menu scaling.
-* TODO: Settings two column collapses to one in smaller windows.
-* TODO: File size is checked and errors out before uploading.
 * Downloading / decrypting now appears again.
 * Empty pastes show something (instead of erroring).
-* TODO: Clicking on a menu entry in mobile closes the menu.
 * Plain / no highlighting option for pasting.
 * User set highlighting types no longer change / detect.
+* Wording changes in about page.
+* TODO: Mobile menu scaling.
+* TODO: Settings two column collapses to one in smaller windows.
+* TODO: Clicking on a menu entry in mobile closes the menu.
+* TODO: File size is checked and errors out before uploading.
 * TODO: IRC highlighting options.
-* TODO: Wording changes in about page.
 
 ## 0.1.2
 * s3-upload uploaded files under dist, now it does not
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,10 +1,11 @@
 <template>
   <div style="width: 100%; height: 100%">
     Pasty is an anonymous, small file and code storage system written in Javascript. All
-    pastes are encrypted client-side before transmission for storage. The
-    corresponding key to each paste never leaves the client, rendering server knowledge
-    of the paste's existence useless. Submitter pastes are not tied to any identifier
-    save the server-provided key.<br />
+    pastes are encrypted client-side before transmission for storage. Submitter pastes are
+    solely tied to a server-provided identifier and are hosted from a public S3 bucket. The
+    corresponding AES key to each paste never leaves the client, rendering server knowledge
+    of the paste's existence useless. No identifying information about the paste's origin is
+    retained.<br />
     <br />
 
     <span class="icon-github-squared"></span>

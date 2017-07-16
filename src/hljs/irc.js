f060d864d2254fa0eee50d53de634f01d1dda3be
Billy Rhoades <billyarhoades@gmail.com>
2017-07-16 13:58:58 -0500

    Public path fix for local development, irc highlighting updated

--- src/hljs/irc.js
+++ src/hljs/irc.js
@@ -4,6 +4,8 @@ Authors: Swant <hljs@swant.pw>
          Billy Rhoades <billyarhoades@gmail.com>
 Website: https://swant.pw/
 Category: misc
+   
+Mostly meant to match weechat raw output.
 */
 
 export default function(hljs) {
@@ -13,44 +15,81 @@ export default function(hljs) {
   contains: [
     {
       className: 'meta',
-      begin: '^\\[', end: '\\]'
-    },
-    {
-      className: 'number',
-      begin: '\\[[0-9]\{2\}:', end: '[0-9]\{2\}:[0-9]\{2\}\\]'
-    },
-    {
-      className: 'meta',
-      begin: '^[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}', end: '.',
-      returnBegin: true,
-      relevance: 3
-    },
-    {
-      className: 'title',
-      begin: ' -', end: '- '
-    },
-    {
-      className: 'title',
-      begin: '^-', end: '- '
-    },
-    {
-      className: 'built_in',
-      begin: ' <', end: '> ',
+      begin: /^\[?[0-9]{2}:[0-9]{2}:[0-9]{2}\]?/, end: /$/,
+      relevance: 8,
       contains: [
+        // Channel info
         {
-          className: 'symbol',
-          begin: '[\\^~@%]{1}',
-          returnBegin: true,
-          end: '.',
-          relevance: 5
-        }
+          className: 'title',
+          begin: '-- ', end: /$/,
+          endsParent: true,
+        },
+        // Action
+        {
+          className: 'title',
+          begin: /\*\s/, end: /$/,
+          endsParent: true,
+          contains: [
+            {
+              // their name
+              className: 'built_in',
+              begin: /\w+/,
+              beginEnds: true,
+            },
+            {
+              // action text
+              className: 'subst',
+              endsParent: true,
+              begin: /\s/, end: /$/,
+              contains: [
+                // timestamp
+                {
+                  className: 'meta',
+                  begin: /\[/, end: /\]/
+                },
+              ],
+            },
+          ],
+        },
+        // message
+        {
+          className: 'built_in',
+          begin: ' <', end: /$/,
+          endsParent: true,
+          contains: [
+            {
+              // someone's handle
+              // a special symbol, like op/halfop
+              className: 'symbol',
+              begin: /[~@%+]{1}/, end: '.',
+              returnBegin: true,
+              relevance: 5,
+            },
+            {
+              // their name
+              className: 'built_in',
+              begin: /[^~@%+\ ]/, end: '>',
+            },
+            {
+              // message
+              className: 'subst',
+              begin: ' ', end: /$/,
+              endsParent: true,
+              contains: [
+                // timestamp
+                {
+                  className: 'meta',
+                  begin: /\[/, end: /\]/
+                },
+              ],
+            },
+
+          ],
+        },
+
       ],
-      relevance: 3
     },
-    {
-      className: 'built_in',
-      begin: '^<', end: '> '
-    }
-  ]
-}
+
+  ]}
+
 }

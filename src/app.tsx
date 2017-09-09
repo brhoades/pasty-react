72c233c418667002cd1738323565b784975d7317
Billy Rhoades <billyarhoades@gmail.com>
2017-09-09 15:50:51 -0500

    IRC language is now registered w/ hljs.

--- src/app.tsx
+++ src/app.tsx
@@ -8,6 +8,7 @@ import { Store } from "redux";
 import { readSettings } from "./actions/creators";
 import CreatePaste from "./containers/createpaste";
 import ViewPaste from "./containers/viewpaste";
+import irc from "./hljs/irc";
 import SettingsLoader from "./loaders/settings";
 import { IReducer } from "./reducers/index";
 import Router from "./router";
@@ -17,6 +18,9 @@ import createStore from "./store";
 const App = () => {
   const history: History = createHistory();
   const store: Store<IReducer> = createStore(history);
+
+  // FIXME: Put this somewhere else?
+  hljs.registerLanguage("irc", irc);
 
   return (
     <Provider store={store}>
